443001243760fd80591ed9b5a9441686b72ca21a
Greiso Faulim <gfaulim@yahoo.com.br>
2022-02-22 20:30:15 -0300

    comitando o projeto

---
+++ src/app/mural-vagas/mural-vagas.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { VagasService } from '../vagas.service';
+import { Vaga } from '../models/Vagas.model';
+
+@Component({
+  selector: 'app-mural-vagas',
+  templateUrl: './mural-vagas.component.html',
+  styleUrls: ['./mural-vagas.component.css']
+})
+export class MuralVagasComponent implements OnInit {
+
+  public vagas: Vaga[] = [];
+
+  constructor(private _vagasService: VagasService) { }
+
+  ngOnInit(): void {
+    this.listarVagas();
+  }
+
+  listarVagas(){
+    this._vagasService.getVagas()
+      .subscribe(
+        retornaVaga => {
+          this.vagas = retornaVaga.map(
+            item=>{
+              return new Vaga(
+                item.id,
+                item.nome,
+                item.foto,
+                item.descricao,
+                item.salario
+              );
+            }
+          )
+        }
+      )
+  }
+}
